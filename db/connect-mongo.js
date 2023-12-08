5069f494705ea22cf159162c9f53bf98fb01f021
Darwin Daniels <damdt18@gmail.com>
2023-12-07 19:03:39 -0500

    quite las credenciales de mongo en el const

--- db/connect-mongo.js
+++ db/connect-mongo.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 const getConnection = async () => {
     try {
-        const url = 'mongodb://darwindanielsdt:crsalamfjts123@ac-itoce6w-shard-00-00.ibobb5f.mongodb.net:27017,ac-itoce6w-shard-00-01.ibobb5f.mongodb.net:27017,ac-itoce6w-shard-00-02.ibobb5f.mongodb.net:27017/jwt_g34?ssl=true&replicaSet=atlas-12r1yp-shard-0&authSource=admin&retryWrites=true&w=majority';
+        const url = '';
 
         await mongoose.connect(url);
         console.log('conexion exitosa');
